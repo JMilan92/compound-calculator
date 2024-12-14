724b80e443b219622394062035033e4d7593ab62
Milan Jovicic <milanjovicic@live.com>
2024-12-14 12:45:44 +0100

    feat(app): setting  up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import Header from './components/layout/Header';
+import Header from "components/layout/Header";
+
+import { createGlobalStyle } from "styled-components";
+
+// Import Google Fonts using @import
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Manrope:wght@200..800&display=swap');
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: "Manrope", sans-serif;
+    background-color: #f9f9f9;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 700;
+  }
+`;
 
 function App() {
   return (
-
-    <Header />
-
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
 }
 
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,14 @@
+import { styled } from "styled-components";
 
+const HeaderStyleComponent = styled.header`
+    background: red;
+`;
 const Header = () => {
-    return (
-        <header>
-            <h1>My Header</h1>
-        </header>
-    );
-}
+  return (
+    <HeaderStyleComponent>
+        <h1>My Header</h1>
+    </HeaderStyleComponent>
+  );
+};
 
 export default Header;
